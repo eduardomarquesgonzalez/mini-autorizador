a45f2d89cb3fbbab5f50cde3cb9dff65fae707d0
eduardomarquesgonzalez <eduardomarquesgonzalezgmail.com>
2022-05-12 17:33:30 -0400

    Criando o projeto inicial

---
+++ src/main/java/br/com/vrbeneficio/MiniAutorizadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.vrbeneficio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vrbeneficio/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vrbeneficio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
